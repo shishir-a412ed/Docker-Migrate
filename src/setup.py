dd632731c280946a4ad170c88ccc30359c282809
Shishir Mahajan <shishir.mahajan@redhat.com>
2015-10-15 18:33:17 -0400

    Added setup.py for docker-migrate
    Signed-off-by: Shishir Mahajan <shishir.mahajan@redhat.com>

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+setup.py for docker-migrate.
+"""
+
+# Author: Shishir Mahajan <shishir.mahajan@redhat.com>
+
+from setuptools import setup
+
+setup(name="docker-migrate", scripts=["docker_migrate"],
+      version='1.0',
+      description="Tool for migrating docker from one backend storage to another",
+      author="Shishir Mahajan", author_email="shishir.mahajan@redhat.com",
+      packages=["dockermigrate"])
